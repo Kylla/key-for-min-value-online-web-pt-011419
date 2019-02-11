0396129a0acc663567902172811ad42ee3d1389e
Kylla Harris <kylla.harris@gmail.com>
2019-02-11 02:10:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(hardware_store)
+  hardware = {"washers" => 12, "nuts" => 10, "bolts" => 18}
 
 end
