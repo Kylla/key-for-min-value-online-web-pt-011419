cba10287a23cc343472797a85abccd348ea033fd
Kylla Harris <kylla.harris@gmail.com>
2019-02-11 05:55:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 
 def key_for_min_value(name_hash)
-  name_hash.each do |item, cost|
-    if cost == 0 
+  name_hash.each do |key, value|
+    if value == 0 
       return nil
-    else cost > 0 
-      return item
+    else value > 0 
+      return key
     end
       
     
