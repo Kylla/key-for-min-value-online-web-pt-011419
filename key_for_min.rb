fe0833890107cebbc47a7081c1f2e6d5f32c17fa
Kylla Harris <kylla.harris@gmail.com>
2019-02-11 06:48:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
  smallest_val = nil
  smallest_key = nil 
   name_hash.each do |key, value|
-  smallest_val == nil || value < smallest_key
+  if smallest_val == nil || value < smallest_key
       smallest_key = key 
       smallest_val = val 
     end
